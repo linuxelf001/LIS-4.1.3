f4450a33306b25482b4520361ed9edccd21eada7
Rakesh Ginjupalli <ginjupallirakesh@gmail.com>
2017-05-05 13:22:15 -0700

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,7 +3,7 @@
 LIS_TEMP='/tmp'
 REQUIRED_MB=250
 REQUIRED_ROOT_MB=100
-URL='https://download.microsoft.com/download/7/6/B/76BE7A6E-E39F-436C-9353-F4B44EF966E9/lis-rpms-4.1.3.tar.gz'
+URL='https://download.microsoft.com/download/6/8/F/68FE11B8-FAA4-4F8D-8C7D-74DA7F2CFC8C/lis-rpms-4.2.0.tar.gz'
 
 #function that checks for free space at a path
 space_check() {
